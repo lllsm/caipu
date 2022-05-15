2ff41938a2475ecfa33c7f8527d81eef1e5d24e5
lllsm <2331222421@qq.com>
2022-05-15 23:03:12 +0800

    2022-5-15 23:03:20

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ App({
         appid: 1,
         apiurl: "https://api.jxcaipu.com/miniapp/v2.0.4/",
         shareinfo: {
-            title: "荷包蛋菜谱",
+            title: "荷包蛋",
             content: "精心挑选每一道美食菜谱，是下厨房必备家常菜谱大全。",
             picurl: "https://img0.jxcaipu.com/mobile/public/images/logo_256.png?v=1"
         },
@@ -25,9 +25,9 @@ App({
     },
     getDefaultPageInfo: function(i, t) {
         return {
-            title: i || "荷包蛋菜谱,每一道菜都是精细之选,做最好的美食分享网站,提供优质的家常菜谱大全",
+            title: i || "荷包蛋,每一道菜都是精细之选,做最好的美食分享网站,提供优质的家常菜谱大全",
             keywords: "美食,菜谱,烹饪,食谱,家常菜谱,美食网站",
-            description: "荷包蛋菜谱为您提供最全、最优质的中文菜谱做法,丰富的菜谱大全让您轻松地学会怎么做美食，展现自己的高超厨艺，与千万会员一同分享美味的人生",
+            description: "荷包蛋为您提供最全、最优质的中文菜谱做法,丰富的菜谱大全让您轻松地学会怎么做美食，展现自己的高超厨艺，与千万会员一同分享美味的人生",
             image: t || this.conf.shareinfo.picurl
         };
     },
